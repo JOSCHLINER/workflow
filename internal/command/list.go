6a28bede553920a10147f3c11dd82d7c33ab915d
joschliner <joschliner@users.noreply.github.com>
2024-03-26 13:12:52 +0000

    added command to list tasks

---
+++ internal/command/list.go
@@ -0,0 +1,28 @@
+package command
+
+import (
+	"workflow/internal/handler"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().Bool("active", false, "List only the active tasks.")
+}
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "used to list all tasks",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		onlyActive, _ := cmd.Flags().GetBool("active")
+
+		if onlyActive == true {
+			handler.ListActiveTasks()
+		} else {
+			handler.ListAllTasks()
+		}
+
+	},
+}
