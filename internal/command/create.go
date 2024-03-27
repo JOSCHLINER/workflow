a14475cf56a820a6e84dc6e3978b987799d1c298
joschliner <joschliner@users.noreply.github.com>
2024-03-27 22:06:41 +0000

    added create command

---
+++ internal/command/create.go
@@ -0,0 +1,27 @@
+package command
+
+import (
+	"fmt"
+	"workflow/internal/handler"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+	createCmd.PersistentFlags().String("name", "", "Name of the task.")
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Command to create a task.",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		name, _ := cmd.Flags().GetString("name")
+		if name == "" {
+			fmt.Println("Please provide a name for the task!")
+			return
+		}
+		handler.CreateTask(name)
+	},
+}
