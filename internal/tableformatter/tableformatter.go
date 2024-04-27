fe7f8ab18e81a05715f489b07528cc1d2d0a9a8d
joschliner <joschliner@users.noreply.github.com>
2024-04-27 17:16:29 +0200

    moved the the package into one file

--- internal/tableformatter/tableformatter.go
+++ internal/tableformatter/tableformatter.go
@@ -1,5 +1,10 @@
 package tableformatter
 
+import (
+	"fmt"
+	"strings"
+)
+
 type TableFormatter struct {
 	Options TableFormatterOptions
 	Spacing []int
@@ -14,6 +19,25 @@ func getDefaultTableFormatter() *TableFormatter {
 	}
 }
 
+type TableFormatterOptions struct {
+	onlyActive bool
+	padding    int
+}
+
+type TableOptions func(*TableFormatterOptions)
+
+func OnlyActiveTasks() TableOptions {
+	return func(tfo *TableFormatterOptions) {
+		tfo.onlyActive = true
+	}
+}
+
+func SetPadding(padding int) TableOptions {
+	return func(tfo *TableFormatterOptions) {
+		tfo.padding = padding
+	}
+}
+
 func NewTableFormatter(opts ...TableOptions) *TableFormatter {
 
 	printer := getDefaultTableFormatter()
@@ -22,4 +46,50 @@ func NewTableFormatter(opts ...TableOptions) *TableFormatter {
 	}
 
 	return printer
+}
+
+func (tf *TableFormatter) ConstructTable(items [][]string) string {
+	tf.getSpacing(items)
+
+	out := tf.constructDivider() + tf.constructRow(items[0]) + tf.constructDivider()
+	for i := 1; i < len(items); i++ {
+		out += tf.constructRow(items[i])
+	}
+
+	return out + tf.constructDivider()
+}
+
+func (tf *TableFormatter) getSpacing(items [][]string) {
+	tf.Spacing = make([]int, len(items[0]))
+
+	for _, row := range items {
+		for c, column := range row {
+
+			if n := len(column); n > tf.Spacing[c] {
+				tf.Spacing[c] = n
+			}
+
+		}
+	}
+}
+
+func (tf *TableFormatter) constructRow(items []string) string {
+
+	padding, line := strings.Repeat(" ", tf.Options.padding), ""
+	for i, item := range items {
+		filler := strings.Repeat(" ", tf.Spacing[i]-len(item)+1)
+		line += fmt.Sprintf("| %s%s%s", padding, item, filler)
+	}
+
+	return line + "|\n"
+}
+
+func (tf *TableFormatter) constructDivider() string {
+
+	line := "+"
+	for _, spacing := range tf.Spacing {
+		line += strings.Repeat("-", tf.Options.padding+spacing+2) + "+"
+	}
+
+	return line + "\n"
 }
